dbc26d55d9b955a2d898c80b06939d0ce6bad0c0
Albert L.M <wipr77@gmail.com>
2022-08-18 16:43:09 +0200

    ar

---
+++ alumnos.py
@@ -0,0 +1,27 @@
+
+class Alumno:
+    nombre: str
+    nota: int
+    
+    def __init__(self, nombre, nota): 
+        self.nombre=nombre
+        self.nota=nota
+        
+    def esAprobado(self) -> bool:
+        if(self.nota>=5):
+            return True
+        else:
+            return False
+ 
+    def getDatos(self):
+        aprobado: str = " y está suspendido"
+        if(self.esAprobado()):
+            aprovado = " y está aprobado"
+            
+        return "Alumno " + self.nombre + " con la nota " + str(self.nota) + aprobado
+
+
+
+alumno = Alumno("Juanjo",6)
+print(alumno.getDatos())
+ 
